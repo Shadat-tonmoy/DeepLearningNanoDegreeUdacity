8ec79bcc8d63061827e3b6e146a0a670e731d265
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-03-25 11:39:58 +0600

    neural network training computation function for forward and back prop is implemented, next to train it

--- NeralNetworks/handWrittenDigitRecognition/neural_network_for_digit_recognition.py
+++ NeralNetworks/handWrittenDigitRecognition/neural_network_for_digit_recognition.py
@@ -40,22 +40,22 @@ def neural_network_model(data):
 
     hidden_layer_1 = {
         'weights': tf.Variable(tf.random_normal([784,num_of_nodes_in_hl1])),
-        'biases': tf.Variable(tf.random_normal(num_of_nodes_in_hl1))
+        'biases': tf.Variable(tf.random_normal([num_of_nodes_in_hl1]))
     }
 
     hidden_layer_2 = {
         'weights': tf.Variable(tf.random_normal([num_of_nodes_in_hl1, num_of_nodes_in_hl2])),
-        'biases': tf.Variable(tf.random_normal(num_of_nodes_in_hl2))
+        'biases': tf.Variable(tf.random_normal([num_of_nodes_in_hl2]))
     }
 
     hidden_layer_3 = {
         'weights': tf.Variable(tf.random_normal([num_of_nodes_in_hl2, num_of_nodes_in_hl3])),
-        'biases': tf.Variable(tf.random_normal(num_of_nodes_in_hl3))
+        'biases': tf.Variable(tf.random_normal([num_of_nodes_in_hl3]))
     }
 
     output_layer = {
         'weights': tf.Variable(tf.random_normal([num_of_nodes_in_hl3, num_of_classes])),
-        'biases': tf.Variable(tf.random_normal(num_of_classes))
+        'biases': tf.Variable(tf.random_normal([num_of_classes]))
     }
 
     # computation performed in each layer
@@ -73,6 +73,57 @@ def neural_network_model(data):
     output= tf.add(tf.matmul(layer_3,output_layer['weights']),output_layer['biases'])
 
     return output
+
+def train_neural_network(x):
+
+    prediction = neural_network_model(x) # getting the predicted value for the input data  x
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(prediction,y)) # cross entropy with logits is cost function to
+    # calculate the difference between the prediction and  the known label in the dataset. Boths are in one hot format
+
+    optimizer = tf.train.AdamOptimizer().minimize(cost) # using adam optimizer minimize the cost
+
+    num_of_epochs = 10      # number of epoch
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables) #start a tf session to perform computatitonal operation
+
+        for epoch in range(num_of_epochs): #looping through each epoch to train the network
+            epoch_loss = 0
+
+            # looping through the number of data to train in a single epoch. Here _ indicates a variable that we don't care about its name. Dividing total number of example by batch size we get the number of training example in a single epoch.
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                # getting the splitted training example with label. These preprocessing is already done by mnist. Real world data need to be preprocessed.
+                epoch_x,epoch_y = mnist.train.next_batch(batch_size)
+                _,c = sess.run([optimizer,cost],free_dict={x:epoch_x,y:epoch_y})
+                #adding the cost denoted by c to epoch loss after each epoch
+                epoch_loss += c
+            print("Epoch ",epoch,' Completed out of ',num_of_epochs,' Epoch Loss ',epoch_loss)
+
+        correct = tf.equal(tf.arg_max(prediction,1),tf.arg_max(y,1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+
+        print("Accuracy ",accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
+
+train_neural_network(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
