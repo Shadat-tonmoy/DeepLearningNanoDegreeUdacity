e77f159046903cbb9be19d56fff5f5b5099df703
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-03-27 09:55:45 +0600

    stop word filteration using nltk is done

---
+++ NeralNetworks/projects/04.SentimentAnalysis/NLTK/02.StopWords.py
@@ -0,0 +1,28 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize,sent_tokenize
+
+'''
+
+Stop words are basically words like a, the, and, for etc
+
+For natural language processing these types of words should and has to be eliminated from sentences
+
+NLTK helps greatly in removing these types of words from sentences
+
+'''
+
+example_sentence = "Welcome to a Natural Language Processing tutorial series, using the Natural Language Toolkit, or NLTK, module with Python."
+
+stop_words = set(stopwords.words("english"))
+
+# filtered_sentence = []
+
+words = word_tokenize(example_sentence)
+
+# for word in words :
+#     if word not in stop_words:
+#         filtered_sentence.append(word)
+
+filtered_sentence = [word for word in words if word not in stop_words]
+
+print(filtered_sentence)
