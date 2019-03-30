bc0fb6bfe04b487a71467a4b39e9ec2cf4a49f15
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-03-30 12:31:00 +0600

    explored the corpus of nltk

---
+++ NeuralNetworks/projects/04.SentimentAnalysis/NLTK/08.Corpora.py
@@ -0,0 +1,14 @@
+from nltk.corpus import movie_reviews
+from nltk.tokenize import sent_tokenize
+
+'''
+
+Corpora is basically corpus of Data. NLTK has a bunch of built in data set in its corpus packagge
+
+'''
+
+sample_text = movie_reviews.raw("neg/cv000_29416.txt")
+
+sentence_tokens = sent_tokenize(sample_text)
+
+print(sentence_tokens[1:10])
