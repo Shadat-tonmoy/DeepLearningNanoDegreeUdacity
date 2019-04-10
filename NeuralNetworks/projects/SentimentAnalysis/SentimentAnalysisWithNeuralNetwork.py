b7d8dbcb35dbe2d772b9d2a1477f489bbc3892d0
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2019-04-10 13:13:56 +0600

    trained the neural network model successfully...need a larger  dataset for better accuracy

--- NeuralNetworks/projects/SentimentAnalysis/SentimentAnalysisWithNeuralNetwork.py
+++ NeuralNetworks/projects/SentimentAnalysis/SentimentAnalysisWithNeuralNetwork.py
@@ -5,12 +5,12 @@ from PreprocessDataset import createFeatureSetAndLabel
 train_x, train_y, test_x, test_y = createFeatureSetAndLabel(['dataset/pos.txt','dataset/neg.txt'])
 
 
-for x in train_x:
-    print("OneHotData ",x," Size ",len(x))
+# for x in train_x:
+#     print("OneHotData ",x," Size ",len(x))
 
-num_of_nodes_in_hidden_layer_1 = 500
-num_of_nodes_in_hidden_layer_2 = 500
-num_of_nodes_in_hidden_layer_3 = 500
+num_of_nodes_in_hidden_layer_1 = 800
+num_of_nodes_in_hidden_layer_2 = 800
+num_of_nodes_in_hidden_layer_3 = 800
 
 
 num_of_classes = 2                                                      # positive and negative
@@ -20,7 +20,7 @@ x = tf.placeholder('float', [None, len(train_x[0])])
 y = tf.placeholder('float')
 
 
-def neural_network_model(data):
+def neuralNetworkModel(data):
 
     hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([len(train_x[0]), num_of_nodes_in_hidden_layer_1])),
                       'biases': tf.Variable(tf.random_normal([num_of_nodes_in_hidden_layer_1]))}
@@ -46,6 +46,53 @@ def neural_network_model(data):
     output = tf.add(tf.matmul(hidden_layer_3_output,output_layer['weights']),output_layer['biases'])
 
     return output
+
+def trainNeuralNetwork(x):
+
+    prediction = neuralNetworkModel(x)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=prediction))
+
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    num_of_epochs = 10
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        for epoch in range (num_of_epochs):
+            epoch_loss = 0
+            i = 0
+            while i < len(train_x):
+                start = i
+                end = i+batch_size
+                batch_x = train_x[start:end]
+                batch_y = train_y[start:end]
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+
+                epoch_loss += c
+                i += batch_size
+            print("Epoch ",epoch, " Completed out of ",num_of_epochs, " Training Loss ",epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print("Accuracy : ", accuracy.eval({x:test_x, y:test_y}))
+
+
+trainNeuralNetwork(x)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
